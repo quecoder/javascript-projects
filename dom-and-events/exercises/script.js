f18acf1493fc57bee53c50617278ca9f527baac5
Quinton Kornegay <quintonkornegay@Aleighas-MacBook-Air.local>
2023-11-09 18:55:42 -0600

    Aborted and headed home

--- dom-and-events/exercises/script.js
+++ dom-and-events/exercises/script.js
@@ -3,7 +3,24 @@ function init () {
     const button = document.getElementById("liftoffButton");
     const paragraph = document.getElementById("statusReport");
 
-    // Put your code for the exercises here.
+    button.addEventListener('click', event => {
+        paragraph.innerHTML = 'Houston! We have liftoff!';
+     });
+
+     missionAbort.addEventListener('mouseover', function (event) {
+       event.target.style.backgroundColor = "red";
+     });
+
+     missionAbort.addEventListener("mouseout", function (event) {
+        event.target.style.backgroundColor = "";
+     });
+
+     missionAbort.addEventListener('click', function (event) {
+        window.confirm ("Are you sure you want to abort the mission?")
+        if (window.confirm = true) {
+            paragraph.innerHTML = "Mission aborted! Space shuttle returning home."
+        };
+     });
     
 }
 
